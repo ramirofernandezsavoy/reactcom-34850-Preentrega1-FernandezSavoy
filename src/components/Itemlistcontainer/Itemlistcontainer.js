21499c17592c010e74e027c43ff9abfd8f14b10a
Ramiro Savoy <rawii.k@gmail.com>
2022-12-23 14:25:35 -0300

    async update

--- src/components/Itemlistcontainer/Itemlistcontainer.js
+++ src/components/Itemlistcontainer/Itemlistcontainer.js
@@ -4,55 +4,60 @@ import "../styles/Itemlistcontainer.css";
 import { useParams } from "react-router-dom";
 import libros from "../../data/libros";
 import { useState, useEffect } from "react";
+import Spinner from "../Spinner/Spinner";
 
 const ItemListContainer = ({ greeting }) => {
-const { categoria } = useParams();
-const res = categoria ? libros.filter((item) => item.categoria === categoria) : libros
-const [resultado, setResultado] = useState()
-console.log(res)
+  const { categoria } = useParams();
+  const res = categoria
+    ? libros.filter((item) => item.categoria === categoria)
+    : libros;
+  const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-
-  setTimeout(() => {
+  useEffect(() => {
     
-  }, 2000);
-
-  return () => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    console.log(loading)
     
-  }
-}, [])
 
+    return () => {};
+  }, []);
 
   return (
-    <div className="item-list-container">      
+    <div className="item-list-container">
       <div>
         <p className="m-2 fs-5 badge bg-danger text-wrap">
           Bienvenido, {greeting}.
         </p>
       </div>
-      <div className="container text-center">        
-        <div className="wrapper">          
-          {res.map(
-            (
-              { id, titulo, autor, categoria, img, precio, btnText }, 
-              index
+      <div className="container text-center">
+        <div className="wrapper">
+          {loading ? (
+            <Spinner />
+          ) : (
+            res.map(
+              (
+                { id, titulo, autor, categoria, img, precio, btnText },
+                index
               ) => (
-              <Cards
-                key={index}
-                id={id}
-                titulo={titulo}
-                autor={autor}
-                categoria={categoria}
-                img={img}
-                precio={precio}
-                btnText={btnText}
-              />
+                <Cards
+                  key={index}
+                  id={id}
+                  titulo={titulo}
+                  autor={autor}
+                  categoria={categoria}
+                  img={img}
+                  precio={precio}
+                  btnText={btnText}
+                />
+              )
             )
-          )}          
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default ItemListContainer;
